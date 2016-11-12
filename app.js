211cad6c16e3f0087d099a4a82edfe92cbd9b8e5
nilfit <nils.fitinghoff@gmail.com>
2016-11-12 16:17:54 +0100

    remove some includes that no longer existed

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var express = require('express');
-// var consolidate = require('consolidate')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -18,9 +14,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', index);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
