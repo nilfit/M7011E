f42780dcc754ee6d0b7acc4422637940761d71f3
Riwzi <rickardnordlander@hotmail.com>
2016-12-02 14:23:09 +0100

    Minor fixes

--- src/menu.jsx
+++ src/menu.jsx
@@ -7,7 +7,7 @@ export default class Menu extends React.Component{
       <div className="menu">
         <ul>
           <li><h1>Birdsong</h1></li>
-         <li><Link to="/login">Sign in</Link></li>
+          <li><Link to="/login">Sign in</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/profile/1">Profile</Link></li> {/*Should link to the current user's profile. '1' is a test value*/}
           <li><Link to="/feed">Feed</Link></li>
