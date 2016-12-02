6c95a4f75502d7b67f9cea39092d974f37a98e4d
nilfit <nils.fitinghoff@gmail.com>
2016-12-02 14:31:50 +0100

    move api secrets to a file separate from the code

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var db = require('../db');
+var fs = require('fs');
 
 router.get('/google',
   passport.authenticate('google', { scope: ['openid'] }));
@@ -21,13 +22,21 @@ router.get('/logout', (req, res) => {
   // this will be easier when there is ui for it
 });
 
+// load the api client ids and secrets
+// var conf;
+// fs.readFile('../secret/passport.json', 'utf8', (err, data) => {
+//   if (err) throw err;
+//   conf = JSON.parse(data
+// });
+var conf = JSON.parse(fs.readFileSync('./secret/passport.json', 'utf8'));
+
 // Use the GoogleStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
 passport.use(new GoogleStrategy({
-    clientID: "197500185071-lptrsekcf4fa4mjmi7kf2p86k9nebjt4.apps.googleusercontent.com",
-    clientSecret: "Fw_H3S4OBX4t2i9M_A0rtUMX",
+    clientID: conf.google.clientID,
+    clientSecret: conf.google.clientSecret,
     callbackURL: "/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
