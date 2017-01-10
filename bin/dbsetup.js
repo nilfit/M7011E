578eff7f04cb0ca492850cc18224dfbff0073fdc
nilfit <nils.fitinghoff@gmail.com>
2017-01-10 16:40:25 +0100

    fix error when dbsetup is run on an empty database
    can't drop an index if the collection doesn't exist

--- bin/dbsetup.js
+++ bin/dbsetup.js
@@ -8,8 +8,6 @@ co(function* () {
   var db = yield MongoClient.connect(url);
 
   var user = db.collection('user');
-  console.log("drop indexes from user")
-  yield user.dropIndexes();
   console.log("index user collection with googleid");
   var r = yield user.createIndex({'identity.googleid':1},
     {unique: true, sparse: true});
@@ -21,15 +19,9 @@ co(function* () {
   console.log(r);
 
   var follow = db.collection('follow');
-  console.log("drop indexes from follow")
-  yield follow.dropIndexes();
   console.log("index follow collection by follower");
   var r = yield follow.createIndex({followerid:1, followedid:1},
     {unique: true});
-  // console.log(r);
-  // console.log("index follow collection by followed");
-  // var r = yield follow.createIndex({followedid:1});
-  // console.log(r);
 
   db.close();
 });
